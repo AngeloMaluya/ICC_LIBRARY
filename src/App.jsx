c1eeab68bdac138bb3d7b4f40d904c60379bd8bd
FoodenShmirt <angelomaluya01@gmail.com>
2026-08-01 20:36:39 +0800

    LOG IN FRONT END

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,16 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { useState } from 'react'
-import './App.css'
+import {LoginSignup} from './login/login.jsx'
+
+function App() {
+
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<LoginSignup />} />
+    </Routes>
+  </Router>
+  )
+}
+
+export default App;
