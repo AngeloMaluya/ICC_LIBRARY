b967122902f00892de82ad0087243b18f3f1d734
FoodenShmirt <angelomaluya01@gmail.com>
2026-08-25 12:00:05 +0800

    TST CMT

--- server/server.js
+++ server/server.js
@@ -32,7 +32,13 @@ const PORT = process.env.PORT || 5000;
 // MIDDLEWARE
 // ========================================
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://icc-library.vercel.app"
+  ],
+  credentials: true
+}));
 
 app.use(express.json());
 
